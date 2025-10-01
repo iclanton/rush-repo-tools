447ab71c133838706d0193ef75de6ac7f727ef2d
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-10-01 11:50:21 -0400

    Expand functionality of the undate-rushstack-projects script to also update the Rush verison in rush.json.

--- src/update-rushstack-projects.ts
+++ src/update-rushstack-projects.ts
@@ -1,18 +1,20 @@
+import { ChildProcess } from "node:child_process";
+
 import { Async, Executable, JsonFile } from "@rushstack/node-core-library";
 import { RushConfiguration } from "@rushstack/rush-sdk";
-import { ChildProcess } from "child_process";
+import type { IRushConfigurationJson } from "@rushstack/rush-sdk/lib/api/RushConfiguration";
 
 async function runAsync(): Promise<void> {
   const rushConfiguration: RushConfiguration =
     RushConfiguration.loadFromDefaultLocation();
 
-  const newVersions: Map<string, string> = new Map();
+  const rushstackDependencies: Set<string> = new Set(["@microsoft/rush"]);
 
   function addDependencies(depSet: Record<string, string> | undefined): void {
     if (depSet) {
       for (const dep of Object.keys(depSet)) {
         if (dep.startsWith("@rushstack/") || dep.startsWith("@microsoft/")) {
-          newVersions.set(dep, "");
+          rushstackDependencies.add(dep);
         }
       }
     }
@@ -25,38 +27,34 @@ async function runAsync(): Promise<void> {
     addDependencies(devDependencies);
   }
 
-  await Async.forEachAsync(
-    newVersions.keys(),
-    async (cyclicDependencyName) => {
-      await new Promise<void>(
-        (resolve: () => void, reject: (error: Error) => void) => {
-          const childProcess: ChildProcess = Executable.spawn("npm", [
-            "view",
-            cyclicDependencyName,
-            "version",
-          ]);
-          const stdoutBuffer: string[] = [];
-          childProcess.stdout!.on("data", (chunk) => stdoutBuffer.push(chunk));
-          childProcess.on("exit", (code: number) => {
-            if (code) {
-              reject(
-                new Error(
-                  `Checking for update for ${cyclicDependencyName} with ${code}`
-                )
-              );
-            } else {
-              const version: string = stdoutBuffer.join("").trim();
-              console.log(
-                `Found version "${version}" for "${cyclicDependencyName}"`
-              );
-              newVersions.set(cyclicDependencyName, version);
-              resolve();
-            }
-          });
+  const newVersions: Map<string, string> = new Map(
+    await Async.mapAsync(
+      rushstackDependencies.keys(),
+      async (dependencyName) => {
+        const childProcess: ChildProcess = Executable.spawn("npm", [
+          "view",
+          dependencyName,
+          "version",
+        ]);
+        const { stdout, stderr } = await Executable.waitForExitAsync(
+          childProcess,
+          {
+            encoding: "utf8",
+            throwOnNonZeroExitCode: true,
+            throwOnSignal: true,
+          }
+        );
+        if (stderr) {
+          throw new Error(
+            `Getting version for ${dependencyName} printed to stderr: ${stderr}`
+          );
         }
-      );
-    },
-    { concurrency: 10 }
+        const version: string = stdout.trim();
+        console.log(`Found version "${version}" for "${dependencyName}"`);
+        return [dependencyName, version];
+      },
+      { concurrency: 10 }
+    )
   );
 
   function updateDependencies(
@@ -94,6 +92,20 @@ async function runAsync(): Promise<void> {
     },
     { concurrency: 50 }
   );
+
+  const rushJson: IRushConfigurationJson = await JsonFile.loadAsync(
+    rushConfiguration.rushJsonFile
+  );
+  const newRushVersion: string | undefined = newVersions.get("@microsoft/rush");
+  if (newRushVersion && rushJson.rushVersion !== newRushVersion) {
+    console.log(
+      `Updating rushVersion in rush.json from ${rushJson.rushVersion} to ${newRushVersion}`
+    );
+    rushJson.rushVersion = newRushVersion;
+    await JsonFile.saveAsync(rushJson, rushConfiguration.rushJsonFile, {
+      updateExistingFile: true,
+    });
+  }
 }
 
 runAsync();
